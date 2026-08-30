0189b24f5b63857bef8ce960b84ab4ae41a6703c
Martin Tracey <1207505+matracey@users.noreply.github.com>
2026-08-29 19:07:39 -0700

    chore(recovery): restore deployed source assets
    Recover the original player headshots and event logo from the generated
    branch under stable typed source paths.

---
+++ src/config/assets.ts
@@ -0,0 +1,23 @@
+import andy from "../assets/players/andy.png";
+import gav from "../assets/players/gav.png";
+import ginny from "../assets/players/ginny.png";
+import jack from "../assets/players/jack.png";
+import kyle from "../assets/players/kyle.png";
+import paul from "../assets/players/paul.png";
+import sam from "../assets/players/sam.png";
+import ste from "../assets/players/ste.png";
+import tim from "../assets/players/tim.png";
+
+export const deployedPlayerImages = {
+  andy,
+  gav,
+  ginny,
+  jack,
+  kyle,
+  paul,
+  sam,
+  ste,
+  tim,
+} as const;
+
+export { default as stagLogo } from "../assets/stag-logo.png";
